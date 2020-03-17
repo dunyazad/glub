e04531db7aeeea96d17ced8b3e4b57b8235fab22
Drgy <drgonjak@gmail.com>
2020-03-17 22:50:19 +0100

    Add glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main() {
-    std::cout << "Running..." << std::endl;
+    GLFWwindow * window;
+
+    if (!glfwInit()) {
+        std::cout << "Unable to initialize glfw!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "glfw initialized..." << std::endl;
+
+    window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Unable to create a window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    std::cout << "Window created..." << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
